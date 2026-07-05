81e649d37ac4d4134a160d3cbcfcb56b412a015a
David Sanabria <davidsst1@outlook.com>
2026-07-05 11:25:35 +0300

    third commit

--- email_alert.py
+++ email_alert.py
@@ -8,12 +8,16 @@ secret_key = os.getenv("API_KEY", "")
 email = os.getenv("EMAIL", "")
 
 def alert(wanted_price):
-    
     resend.api_key = secret_key
     
+    with open("template.html", "r", encoding="utf-8") as file:
+        html_template = file.read()
+    
+    html_content = html_template.format(wanted_price=wanted_price)
+
     r = resend.Emails.send({
         "from": "onboarding@resend.dev",
         "to": email,
-        "subject": "SSD PRICE ALERT",
-        "html": f" I have found the desirable {wanted_price} price for your SDD"
+        "subject": f"🚨 Price Alert: Target Hardware dropped to {wanted_price}€!",
+        "html": html_content
     })
